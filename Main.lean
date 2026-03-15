b45021dab5e7f3ecc8489ca68fe41995f1f42512
Erchius <zeyue_jia@u.nus.edu>
2026-03-15 15:00:38 +0800

    implement data structures

--- Main.lean
+++ Main.lean
@@ -8,296 +8,304 @@ type nodeset
 type vertexset
 type address
 
-
 -- instance round from an abstract type to Nat
 abbrev round := Nat
-abbrev count := Nat
 open Nat
 
 type vertex
--- structure vertex where
---   round: round
---   source: address
---   block: block
---   strong: vertexset
---   weak: vertexset
--- class Vertex (vertex: Type) where
---   round: vertex → round
---   source: vertex → address
---   block: vertex → block
---   strong: vertex → vertexset
---   weak: vertex → vertexset
+class Vertex
+  (vertex address block vertexset: Type)
+where
+  round: vertex → round
+  source: vertex → address
+  block: vertex → block
+  strong: vertex → vertexset
+  weak: vertex → vertexset
 
-mutable relation vertex'fields
-  (v: vertex)
-  (source: address)
-  (r: round)
-  (b: block)
-  (strong: vertexset)
-  (weak: vertexset)
+instantiate v_: Vertex vertex address block vertexset
 
-class VertexSet (vertex: Type) (vset: Type) where
+class VertexSet
+  (vertex vset address block: Type)
+  [v_: Vertex vertex address block vset]
+where
   member (a: vertex) (s: vset) : Prop
   empty: vset
+  empty_with_round: round → vset
+  round: vset → round
 
-  insert (a: vertex) (s: vset): vset
+  empty_with_round_def:
+    ( ∀ (r: round)
+      , round (empty_with_round r) = r
+    ) ∧
+    ( ∀ (a: vertex) (r: round)
+      , ¬ member a (empty_with_round r)
+    )
 
-  insert_has_subset:
-    ∀ (a: vertex) (s: vset),
-      member a (insert a s)
-      ∧ (∀ (b: vertex), member b s → member b (insert a s))
+  insert (a: vertex) (s: vset)
+    (alignment: s = empty ∨ v_.round a = round s)
+    : vset
+
+  insert_contains_a_and_s:
+    ∀ (a: vertex)
+      (s: vset)
+      (alignment:
+        s = empty ∨ v_.round a = round s
+      )
+    , member a (insert a s alignment)
+    ∧ ∀ (b: vertex)
+      , member b s
+      → member b (insert a s alignment)
 
   supermajority (s : vset): Prop
+
+  round_alignment:
+    ∀ (a: vertex) (s: vset)
+    , member a s
+    → v_.round a = round s
 
   empty_is_not_supermajority: ¬ supermajority empty
   empty_is_empty: ∀ (a: vertex), ¬ member a empty
 
-instantiate vset : VertexSet vertex vertexset
+instantiate vset : VertexSet vertex vertexset address block
+
+type path
+class Path
+  (path vertex address block vertexset: Type)
+  [v_: Vertex vertex address block vertexset]
+  [vset: VertexSet vertex vertexset address block]
+where
+  member: vertex → path → Prop
+  exclusion_round:
+    ∀ (p: path)
+      (v1 v2: vertex)
+    , (member v1 p ∧ member v2 p)
+    → v_.round v1 ≠ v_.round v2
+
+  top: path → vertex
+  bottom: path → vertex
+
+  top_bottom_elem:
+    ∀ (p: path)
+    , member (top p) p
+    ∧ member (bottom p) p
+
+  connected:
+    ∀ (p: path)
+    , ∀ (v: vertex)
+      , member v p
+      → v = top p
+      ∨ ∃ (v': vertex)
+        , member v' p
+        ∧ ( vset.member v' (v_.strong v)
+          ∨ vset.member v' (v_.weak v)
+          )
+
+  round_bound:
+    ∀ (p: path) (v: vertex)
+    , member v p
+    → v_.round v ≥ v_.round (bottom p)
+    ∧ v_.round v ≤ v_.round (top p)
+
+
+instantiate path_: Path path vertex address block vertexset
 
 variable (is_byz : address → Prop)
 instantiate nset : NodeSet address is_byz nodeset
 open NodeSet
 
+type view
+class View
+  (view vertex vertexset address block: Type)
+  [v_: Vertex vertex address block vertexset]
+  [vset: VertexSet vertex vertexset address block]
+where
+  current: view → vertexset
+  past: view → Option view
+  round: view → round
 
--- structure DAG'view where
---   current'round'vertices: vertexset
---   past'round: Option DAG'view
-type DAG'view
-relation DAG'view'fields
-  (view: DAG'view)
-  (current: vertexset)
-  (past: Option DAG'view)
+  next (v: view): view
 
+  empty: view
+  empty_view_def:
+    current empty = vset.empty
+    ∧ past empty = none
+    ∧ round empty = 1
+
+  only_empty_view_has_no_past:
+    ∀ (v: view)
+    , past v = none ∨ round v = 1
+    → current v = vset.empty
+
+  next_def:
+    ∀ (v: view)
+    , current (next v) = vset.empty
+    ∧ past (next v) = some v
+    ∧ round (next v) = round v + 1
+
+  height_view:
+    ∀ (v: view)
+    , match past v with
+      | none => round v = 1
+      | some v' => round v = round v' + 1
+
+  positivity:
+    ∀ (v: view)
+    , round v > 0
+
+  round_alignment:
+    ∀ (v: view)
+    , current v = vset.empty
+    ∨ round v = vset.round (current v)
+
+instantiate view_: View view vertex vertexset address block
 
 relation current_round (a: address) (r: round)
 
--- seems impossible to correctly initialize a individual f,
--- so maybe we need to pass f as an argument to every action?
--- individual f : Nat
-
--- how about individual dag'view'map : Std.HashMap round (DAG'view vertexset)
-relation has'view (a: address) (v: DAG'view)
-variable (empty'view: DAG'view)
-relation allocated_view (v: DAG'view)
-relation allocated_vertex (v: vertex)
-
-relation sent (n: address) (r: round)
-relation cached (n: address) (v: vertex)
-relation delivered (n: address) (originator: address) (in_round: round) (v: vertex)
-
-
+relation has'view (a: address) (v: view)
+relation view'slice (a: address) (s: vertexset)
+relation received (a: address) (v: vertex)
 #gen_state
 
-ghost relation ready'for'next'round
-  (a: address)
-  (v: DAG'view)
-  (r: round)
-  (s: vertexset)
-  (p: Option DAG'view)
-  := current_round a r
-  ∧ has'view a v
-  ∧ DAG'view'fields v s p
-  ∧ vset.supermajority s
-
 #print State
+
+def vertex'in'view
+  (v: vertex) (view': Option view): Prop :=
+  match view' with
+  | none => False
+  | some view
+    => vset.member v (view_.current view)
+    ∨ vertex'in'view v (view_.past view)
+  termination_by match view' with
+    | none => 0
+    | some v => view_.round v
+  decreasing_by {
+    cases h: view_.past v' with
+    | none =>
+        simp
+        exact (view_.positivity v')
+    | some v1 =>
+        simp
+        have h1 := view_.height_view v'
+        rw [h] at h1
+        simp at h1
+        rw [h1]
+        simp
+  }
+
+def path'in'dag (p: path) (v: view): Prop :=
+  ∀ (v': vertex)
+  , path_.member v' p
+  → vset.member v' (view_.current v)
+  ∨ vertex'in'view block vertexset address vertex view v' (view_.past v)
 
 after_init {
   current_round A N         := False
   current_round A 0         := True
   has'view A V              := False
-  has'view A empty'view     := True
-  -- view and vertex construction
-  DAG'view'fields V C P     := False
-  DAG'view'fields
-    empty'view
-    vset.empty
-    Option.none             := True
-  allocated_view V          := False
-  allocated_view empty'view := True
-  allocated_vertex V        := False
-  vertex'fields V A R B S W := False
-  sent N R                  := False
-  cached N R              := False
-  delivered N O R V         := False
+  has'view A view_.empty    := True
+  received A V              := False
+  view'slice A S            := False
+}
+
+action advance_round_slice
+  (a: address)
+  (r: round) = {
+  require current_round a r
+  require
+    ∀ (s: vertexset)
+    , view'slice a s
+    ∧ vset.round s = r
+    → vset.supermajority s
+  current_round a r := False
+  current_round a (r + 1) := True
 }
 
 action advance_round
   (a: address)
   (r: round)
-  (v: DAG'view)
-  (s: vertexset)
-  (p: Option DAG'view)
-  (v': DAG'view) = {
-  -- require ready'for'next'round a v r s p
-  /- why does this meet the error `unknown metavariable`
-    require ready'for'next'round a v r s p
-unknown metavariable '?_uniq.58816'
-(kernel) declaration has metavariables 'DagRiderBroadcast.advance_round.genI'
-(kernel) declaration has metavariables 'DagRiderBroadcast.advance_round.genI.eq_1'
-  -/
-
+  (v: view) = {
   require current_round a r
   require has'view a v
-  require DAG'view'fields v s p
-  require vset.supermajority s
-
+  require vset.supermajority (view_.current v)
+  require
+    ∃ (v': vertex)
+    , vset.member v' (view_.current v)
+    ∧ v_.source v' = a
   current_round a r := False
-  current_round a (r+1) := True
+  current_round a (r + 1) := True
   has'view a v := False
-  -- allocate new view and bind to `a`
-  require ¬ allocated_view v'
-  allocated_view v' := True
-  has'view a v' := True
-  DAG'view'fields v' vset.empty (Option.some v) := True
+  has'view a (view_.next v) := True
 }
 
-action construct_vertex
+
+
+action send
   (v: vertex)
   (a: address)
-  (r: round)
-  (b: block)
-  (strong: vertexset)
-  (weak: vertexset)
-  (view: DAG'view)
-  (current: vertexset)
-  (p: Option DAG'view)= {
-  require ¬ allocated_vertex v
-  allocated_vertex v := True
-  require DAG'view'fields view current p
+  (vi: view)
+  (vi': view) = {
+  -- check that the sender owns the view and the vertex
+  require has'view a vi
+  require v_.source v = a
+  -- the vertex differs from the vertices in the view
   require
-    ( p = Option.none ∧ strong = vset.empty) ∨
-    ( ∃ (view': DAG'view)
-    , p = Option.some view'
-    ∧ ∃ p': Option DAG'view
-      , DAG'view'fields view' strong p'
-    )
+    ∀ (v': vertex)
+    , vset.member v' (view_.current vi)
+    → v_.source v' ≠ a
+  -- the strong set should be the parent view's all edges
+  require v_.strong v = match view_.past vi with
+    | none => vset.empty
+    | some past_view => view_.current past_view
+  let vertex'in'view'judgement :=
+    vertex'in'view block vertexset address vertex view
   require
-    ( ∀ (v': vertex)
-      , member v' weak
-      → ∃ (path: vertexset)
-        , ∀ (v1 v2: vertex)
-          , member v1 path ∧ member v2 path
-          →
-    )
-  vertex'fields v a r b strong weak := True
+    ∀ (v0: vertex)
+    , vset.member v0 (v_.weak v)
+    ↔ ¬ ∃ (p: path)
+      , path_.top p = v
+      ∧ path_.bottom p = v0
+      ∧ ∀ (v1: vertex)
+        , path_.member v1 p
+        → (v1 ≠ v0 ∧ vset.member v1 (v_.weak v))
+        ∨ vertex'in'view'judgement v1 (some vi)
+  /- the vertex should be from the current round
+    here we require this alignment, and we will use it as `sorry`
+    in the insertion
+  -/
+  require v_.round v = view_.round vi
+  -- all checks passed, we can send the vertex
+
+  -- vi' is the modified view after vi
+  require
+    view_.current vi'
+    = vset.insert v (view_.current vi)
+    ( by {
+      have round_alignment
+        : v_.round v = view_.round vi := by sorry
+      have h := view_.round_alignment vi
+      rw [round_alignment]
+      cases h with
+      | inl h1 =>
+          rw [h1]
+          simp
+      | inr h2 =>
+          rw [h2]
+          simp
+    })
+  require
+    view_.past vi' = some vi
+
+  has'view a vi := False
+  has'view a vi' := True
 }
-
-
-
--- action send
---   (a: address)
---   (r: round)
---   (b: block)
---   (v: vertex)
---   (view: DAG'view)
---   (c: vertexset)
---   (p: Option DAG'view)
---   (weak: vertexset)= {
---   require ¬ sent a r
---   require has'view a view
---   require DAG'view'fields view c p
---   -- allocate new vertex and bind to `a` and `b`
---   require ¬ allocated_vertex v
---   -- compute strong and weak edges
---   let strong := c
---   sent a r := True
---   require
---     ( ∀ (v': vertex)
---       , member v'c
---       → )
---   construct_vertex v r b strong weak
--- }
 
 action receive
   (a: address)
   (v: vertex)
-  (source: address)
-  (b: block)
-  (r: round)
-  (strong: vertexset)
-  (weak: vertexset) = {
-  require allocated_vertex v
-  require vertex'fields v source r b strong weak
+  (vi: view) = {
+  require has'view a vi
 
 }
-
-safety [round_exist]
-  ( ∀ (a: address), ∃ (r: round),
-    current_round a r )
-safety [round_exist_unique]
-  ( ∀ (a: address) (r1 r2: round),
-    current_round a r1 ∧ current_round a r2
-    → r1 = r2)
-
-safety [view_exist]
-  ( ∀ (a: address), ∃ (v: DAG'view),
-    has'view a v)
-safety [view_exist_unique]
-  ( ∀ (a: address) (v1 v2: DAG'view),
-    has'view a v1 ∧ has'view a v2
-    → v1 = v2)
-
--- view and vertex work like structures
-safety [vertex_structure_exist]
-  ( ∀ (v: vertex),
-    ( ¬ allocated_vertex v
-      ∧ ∀ (r: round)
-          (a: address)
-          (b: block)
-          (s: vertexset)
-          (w: vertexset)
-        , ¬ vertex'fields v a r b s w
-    )
-    ∨ ( allocated_vertex v
-      ∧ ∃ (r: round)
-          (a: address)
-          (b: block)
-          (s: vertexset)
-          (w: vertexset)
-        , vertex'fields v a r b s w
-      )
-  )
-safety [vertex_structure_unique]
-  ( ∀ (v: vertex)
-      (a1 a2: address)
-      (r1 r2: round)
-      (b1 b2: block)
-      (s1 s2: vertexset)
-      (w1 w2: vertexset),
-    vertex'fields v a1 r1 b1 s1 w1
-    ∧ vertex'fields v a2 r2 b2 s2 w2
-    → a1 = a2 ∧ r1 = r2 ∧ b1 = b2 ∧ s1 = s2 ∧ w1 = w2
-  )
-
-
-safety [DAGview_structure_exist]
-  ( ∀ (v: DAG'view),
-    ( ¬ allocated_view v
-      ∧ ∀ (c: vertexset)
-          (p: Option DAG'view)
-        , ¬ DAG'view'fields v c p
-    )
-    ∨ ( allocated_view v
-      ∧ ∃ (c: vertexset)
-          (p: Option DAG'view)
-        , DAG'view'fields v c p
-      )
-  )
-safety [DAGview_structure_unique]
-  ( ∀ (v: DAG'view)
-      (c1 c2: vertexset)
-      (p1 p2: Option DAG'view)
-    , DAG'view'fields v c1 p1
-    ∧ DAG'view'fields v c2 p2
-    → c1 = c2 ∧ p1 = p2
-  )
-
-safety [DAG_contain_only_allocated_vertices]
-  ( ∀ (view: DAG'view) (v: vertex) (c: vertexset) (p: Option DAG'view)
-    , DAG'view'fields view c p
-    ∧ vset.member v c
-    → allocated_vertex v
-  )
 
 #gen_spec
 
