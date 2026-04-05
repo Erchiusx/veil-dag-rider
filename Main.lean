2ecca4aa71872a15463a5cab6e368ca6f6e02257
Erchius <zeyue_jia@u.nus.edu>
2026-04-05 16:23:15 +0800

    use list to replace relations

--- Main.lean
+++ Main.lean
@@ -30,8 +30,8 @@ deriving instance Veil.FinEncodableInjOnly for vertex
 -- instantiate instv: LE $ vertex address block
 -- instantiate decLEv: DecidableRel $ instv.le
 -- instantiate ordv: Ord $ vertex address block
-set_option trace.Meta.synthInstance true in
-deriving instance DecidableEq for vertex
+-- set_option trace.Meta.synthInstance true in
+-- deriving instance DecidableEq for vertex
 
 -- we need to quantify over vertices,
 -- so the round parameter is separated from the vertex type
@@ -86,9 +86,11 @@ function a_deliver_at: address → List (block × round × address × wave)
 -- function recvLog: address → (checked: vertex address block) → Option (round × (message address block vtxs))
 function recvLog: address → List ((vertex address block × round × message address block))
 -- when receiving, set the `recvLog` of the vertex from the r_deliver
-relation voted: address → vertex address block → Bool
+function voted: address → List (vertex address block)
 function waveVertexLeader: address → wave → Option (vertex address block)
 immutable individual f: Nat
+
+individual test_: Nat
 #gen_state
 
 after_init {
@@ -108,57 +110,88 @@ after_init {
 
   -- recvLog I V := none
   recvLog I := []
-  voted I V := false
+  voted I := []
 
   waveVertexLeader I W := none
 }
 
--- action DAG_maintain (i: address) {
---   let g := view i
---   let v
---    :| recvLog i v ≠ none
---     ∧ ¬ vset.contains v g.nodes
---   require recvLog i v ≠ none
---   require ¬ vset.contains v g.nodes
---   let mut ret := g
---   if let some (r, m) := recvLog i v then
---     if (
---       let timecheck := (r ≤ current_round i)
---       let deps := vset.union m.strong m.weak |> vset.toList
---       let depscheck :=
---         ∀ v': vertex address block
---         , v' ∈ deps
---         → vset.contains v' g.nodes
---       let majority_escape := r == 1
---       let majority :=
---         (m.strong |> vset.count) > 2 * f + 1
---       let validity :=
---         ∀ v': vertex address block
---         , v' ∈ m.strong
---         → (recvLog i v')
---           <&> ( fun (r', _) => r' + 1 == r
---           ) = some true
---       ; timecheck
---       ∧ (majority_escape ∨ majority)
---       ∧ validity
---       ∧ depscheck
---     )
---     then
---       let strong_edges
---         := vset.toList m.strong |> List.map (fun v' => edge.mk v v')
---       let weak_edges
---         := vset.toList m.weak |> List.map (fun v' => edge.mk v v')
---       ret := Graph.mk
---         (vset.insert v g.nodes)
---         (strong_edges ++ g.strong)
---         (weak_edges ++ g.weak)
---   view i := ret
--- }
+action DAG_maintain (i: address) {
+  let g := view i
+  -- let v
+  --  :| recvLog i v ≠ none
+  --   ∧ ¬ vset.contains v g.nodes
+  let recv := (recvLog i).mergeSort (fun (_, r, _) (_, r', _) => r < r')
+  view i := recv.foldl (fun ga (v, r, msg) =>
+  if (
+    let timecheck := (r ≤ current_round i)
+    let deps := msg.strong ++ msg.weak
+    let depscheck := deps.all (fun v => v ∈ g.nodes)
+    let majority_escape := r == 1
+    let majority := msg.strong.length > 2 * f + 1
+    let validity := (do
+      let v' <- msg.strong
+      let (c', r', _) <- recvLog i
+      -- guard (v' == c')
+      if v' ≠ c' then [] else
+      pure $ r' + 1 == r
+    ).all (·)
+      ; timecheck
+    && (majority_escape || majority)
+    && validity
+    && depscheck
+  ) then
+    let strong_edges := msg.strong.map (edge.mk v)
+    let weak_edges := msg.weak.map (edge.mk v)
+    Graph.mk
+      (g.nodes.insert v)
+      (strong_edges ++ g.strong)
+      (weak_edges ++ g.weak)
+    else ga ) g
+  -- require recvLog i v ≠ none
+  -- require ¬ vset.contains v g.nodes
+  -- let mut ret := g
+  -- if let some (r, m) := recvLog i v then
+  --   if (
+  --     let timecheck := (r ≤ current_round i)
+  --     let deps := vset.union m.strong m.weak |> vset.toList
+  --     let depscheck :=
+  --       ∀ v': vertex address block
+  --       , v' ∈ deps
+  --       → vset.contains v' g.nodes
+  --     let majority_escape := r == 1
+  --     let majority :=
+  --       (m.strong |> vset.count) > 2 * f + 1
+  --     let validity :=
+  --       ∀ v': vertex address block
+  --       , v' ∈ m.strong
+  --       → (recvLog i v')
+  --         <&> ( fun (r', _) => r' + 1 == r
+  --         ) = some true
+  --     ; timecheck
+  --     ∧ (majority_escape ∨ majority)
+  --     ∧ validity
+  --     ∧ depscheck
+  --   )
+  --   then
+  --     let strong_edges
+  --       := vset.toList m.strong |> List.map (fun v' => edge.mk v v')
+  --     let weak_edges
+  --       := vset.toList m.weak |> List.map (fun v' => edge.mk v v')
+  --     ret := Graph.mk
+  --       (vset.insert v g.nodes)
+  --       (strong_edges ++ g.strong)
+  --       (weak_edges ++ g.weak)
+  -- view i := ret
+}
 
--- action ReliableBroadcast (i: address) (j: address) {
---   let m :| r_bcast i m ≠ none ∧ r_deliver j m i = none
---   r_deliver j m i := r_bcast i m
--- }
+action ReliableBroadcast (i: address) (j: address) {
+  -- let m :| r_bcast i m ≠ none ∧ r_deliver j m i = none
+  let mpack :| mpack ∈ r_bcast i
+  let (m, r) := mpack
+  if (m, i, r) ∈ r_deliver j then return ()
+  -- r_deliver j m i := r_bcast i m
+  r_deliver j := r_deliver j |> List.insert (m, i, r)
+}
 
 action GlobalPerfectCoin (w: wave) {
   if
@@ -182,74 +215,101 @@ action ChooseLeader (i: address) (w: wave) {
   GlobalPerfectCoin w
 }
 
--- action recv (i: address) (m: message address block vtxs) {
---   let j :| r_deliver i m j ≠ none
---   require vset.count m.strong > (2 * f + 1)
---     ∨ (r_deliver i m j <&> (·==1)) == some true
---   let checked := vertex.mk j m.payload.payload
---   recvLog i checked := (r_deliver i m j).map (·, m)
---   DAG_maintain i
--- }
+action recv (i: address) {
+  let output :| output ∈ r_deliver i
+  let (m, j, r) := output
+  -- -- require vset.count m.strong > (2 * f + 1)
+  -- --   ∨ (r_deliver i m j <&> (·==1)) == some true
+  if m.strong.length ≤ (2 * f + 1) ∧ r ≠ 1 then return ()
+  let checked := vertex.mk j m.payload.payload
+  if (checked, r, m) ∈ recvLog i then return ()
+  -- recvLog i checked := (r_deliver i m j).map (·, m)
+  recvLog i := recvLog i |> List.insert (checked, r, m)
+  DAG_maintain i
+}
 
--- action send (i: address) {
---   let r := current_round i
---   let b := a_bcast i r
---   let v := vertex.mk i b
---   let g := view i
---   require
---     ∀ m: message address block vtxs
---     , r_bcast i m ≠ none
---     → r_bcast i m ≠ some r
---   require r ≠ 0
---   require r = 1 ∨
---     ∃ s: nodeSet
---     , nset.supermajority s
---     ∧ ∀ i: address
---       , nset.member i s
---       → ∃ v': vertex address block
---         , vset.contains v' g.nodes
---         ∧ (·.1 == r-1) <$> recvLog i v' = some true
---   let m
---    := message.mk v
---     (vset.filter g.nodes
---       (fun v' => ((·.1 == r-1) <$> recvLog i v') = some true))
---     (vset.filter g.nodes
---       (fun v' => (((fun (r', _) => decide $ r' < r-1) <$> recvLog i v') = some true) ∧ voted i v' = false))
---   r_bcast i m := some r
---   voted i V := vset.contains V g.nodes
---   let strong_edges
---     := vset.toList m.strong |> List.map (fun v' => edge.mk v v')
---   let weak_edges
---     := vset.toList m.weak |> List.map (fun v' => edge.mk v v')
---   view i := Graph.mk
---     (vset.insert v g.nodes)
---     (strong_edges ++ g.strong)
---     (weak_edges ++ g.weak)
---   recvLog i v := some (r, m)
--- }
+action send (i: address) {
+  let r := current_round i
+  let b := a_bcast i r
+  let v := vertex.mk i b
+  let g := view i
+  -- require
+  --   ∀ m: message address block
+  --   , r_bcast i m ≠ none
+  --   → r_bcast i m ≠ some r
+  require
+    r_bcast i |> (List.all · (fun (_, r') => r' ≠ r))
+  require r ≠ 0
+  require r = 1 ∨
+    ∃ s: nodeSet
+    , nset.supermajority s
+    ∧ ∀ i: address
+      , nset.member i s
+      → ∃ v': vertex address block
+        , v' ∈ g.nodes
+        ∧ ((recvLog i).any (fun (checked, r', _) => checked == v' ∧ r' == r-1))
+  let strong := do
+    let node <- g.nodes
+    let (checked, r', _) <- recvLog i
+    if r' == r-1 && node == checked then pure node else []
+  let weak := do
+    let node <- g.nodes
+    let (checked, r', _) <- recvLog i
+    if r' < r-1 && node == checked && (node ∉ voted i) then pure node else []
+  let m := message.mk v strong weak
+    -- (g.nodes.filter
+    --   (fun v' => ((·.1 == r-1) <$> recvLog i v') = some true))
+    -- (g.nodes.filter
+    --   (fun v' => (((fun (r', _) => decide $ r' < r-1) <$> recvLog i v') = some true) ∧ voted i v' = false))
+  -- r_bcast i m := some r
+  r_bcast i := r_bcast i |> List.insert (m, r)
+  -- voted i V := vset.contains V g.nodes
+  voted i := g.nodes.insert v
+  recvLog i := recvLog i |> List.insert (v, r, m)
+  -- let strong_edges
+  --   := vset.toList m.strong |> List.map (fun v' => edge.mk v v')
+  let strong_edges := strong.map (fun v' => edge.mk v v')
+  -- let weak_edges
+  --   := vset.toList m.weak |> List.map (fun v' => edge.mk v v')
+  let weak_edges := weak.map (fun v' => edge.mk v v')
+  -- view i := Graph.mk
+  --   (vset.insert v g.nodes)
+  --   (strong_edges ++ g.strong)
+  --   (weak_edges ++ g.weak)
+  view i := Graph.mk (voted i) (strong_edges ++ g.strong) (weak_edges ++ g.weak)
+  -- recvLog i v := some (r, m)
+}
 
 action getWaveVertexLeader (i: address) (w: wave) {
   require w > 0
-  require current_round i > 4 * (w-1)
+  require current_round i ≥ 4 * w
   ChooseLeader i w
   let g := view i
   let mut ret := waveVertexLeader i w
-  if ret ≠ none then return ()
-  if let some j := waveLeader w then
+  if ret ≠ none then return ret
+  -- if let some j := waveLeader w then
     -- let v
     --   :| v ∈ g.nodes
     --   && (recvLog i |> List.find? (·.1 == v))
     --     <&> (fun (_, r, _) => r == 4 * (w-1) + 1) == some true
     --   && v.source == j
     -- ret := v
-    for v in g.nodes do
-      -- if (recvLog i |> List.find? (·.1 == v)) <&> (fun (_, r, _) => r == 4 * (w-1) + 1) == some true then
-      for (checked, r, msg) in recvLog i do
-        if r == 4 * (w-1) + 1 then
-        if checked == v then
-        if v.source == j then
-          ret := v
-  waveVertexLeader i w := ret
+    -- for v in g.nodes do
+    --   -- if (recvLog i |> List.find? (·.1 == v)) <&> (fun (_, r, _) => r == 4 * (w-1) + 1) == some true then
+    --   for (checked, r, msg) in recvLog i do
+    --     if r == 4 * (w-1) + 1 then
+    --     if checked == v then
+    --     if v.source == j then
+    --       ret := v
+  -- waveVertexLeader i w :=
+  return g.nodes.foldl (fun ret v =>
+        if (recvLog i).any (fun (checked, r, _)
+          => r == 4 * (w-1) + 1
+           ∧ checked == v
+           ∧ some (v.source) == waveLeader w
+        ) then some v else ret
+      ) ret
+  -- waveVertexLeader i w := ret
 }
 
 -- ghost relation strong_path (i: address) (top bot: vertex address block) :=
@@ -315,41 +375,124 @@ action getWaveVertexLeader (i: address) (w: wave) {
 --         , { top := v', bot := v } ∈ g.strong ∪ g.weak
 --     )
 
-
--- action waveReady (i: address) (w: wave) {
---   let vo <- getWaveVertexLeader i w
---   let wd := decidedWave i
---   let mut leaderStack: List $ vertex address block := []
---   let sliceRound := (4: Nat) * w
---   -- let prepare := (do
---   --   let v_ <- vo
---   --   pure $
---   --     ∃ (s: vtxs)
---   --       , vset.count s > 2 * f + 1
---   --       ∧ ∀ (v: vertex address block)
---   --         , vset.contains v s
---   --         → (do
---   --             let (r, _) <- recvLog i v
---   --             pure $ r = sliceRound
---   --               ∧ strong_path i v v_
---   --           ).getD False
---   -- ).getD False
---   -- if ¬ prepare then return ()
---   -- if let some v_ := vo then
---   --   let mut v := v_
---   --   for w' in List.range (w-1 - wd) |> List.map (fun index => w-1-index ) do
---   --     let v' <- getWaveVertexLeader i w'
---   --     if let some v_ := v' then
+-- procedure findLog (i: address) (default v': vertex address block) {
+--   return ((recvLog i).find?
+--     (fun (checked, _, _) => checked == v')).getD (default, 0, message.mk default [] [])
 -- }
 
--- action advanceRound (i: address) {
---   return ()
--- }
+-- set_option trace.Meta.synthInstance true
+def findLog?
+  (default: vertex address block)
+  (logs: List $ (vertex address block × round × message address block))
+  (v': vertex address block)
+  [DecidableEq (vertex address block)] :=
+  (logs.find?
+    (fun (checked, _, _) => decide $ checked = v')).getD (default, 0, message.mk default [] [])
+  -- this `getD` will not be triggered, `default` only passed for type
+
+partial def strong_path
+  (default: vertex address block)
+  (g: Graph address block $ edge address block)
+  (logs: List $ (vertex address block × round × message address block))
+  (u v: vertex address block)
+  [DecidableEq (vertex address block)]
+  : Bool :=
+  if u == v then true else
+  let (u, ur, um) := findLog? default logs u
+  let (v, vr, vm) := findLog? default logs v
+  if ur ≤ vr then false else
+  um.strong.any (fun u' => strong_path default g logs u' v)
+
+partial def path
+  (default: vertex address block)
+  (g: Graph address block $ edge address block)
+  (logs: List $ (vertex address block × round × message address block))
+  (u v: vertex address block)
+  [DecidableEq (vertex address block)]
+  : Bool :=
+  if u == v then true else
+  let (u, ur, um) := findLog? default logs u
+  let (v, vr, vm) := findLog? default logs v
+  if ur ≤ vr then false else
+  (um.strong ++ um.weak).any (fun u' => path default g logs u' v)
+
+action orderVertices (i: address) (vertices: List $ vertex address block) {
+  return ()
+}
+
+action waveReady (i: address) (w: wave) {
+  require w > 0
+  require current_round i ≥ 4 * w
+  let wd := decidedWave i
+  require w > wd
+  -- getWaveVertexLeader i w
+  let wo <- getWaveVertexLeader i w
+  -- let wo := waveVertexLeader i w
+  if wo == none then return ()
+  let g := view i
+  let v? :| v? ∈ g.nodes
+  let v := wo.getD v?
+  -- let vl <- findLog i v v
+  let logs := recvLog i
+  let findLog! := findLog? v logs
+  let vl := findLog! v
+  -- this `getD` will never fail, v is only assisting the type;
+  -- normally we should use the pattern `if let some vl := wo` ,
+  -- but #gen_spec denies that??
+  -- let findLog := fun v' => ((recvLog i).find?
+  --   (fun (checked, _, _) => checked == v')).getD (v, 0, message.mk v [] [])
+    -- only call with nodes in g
+  let mut leaderStack: List $ vertex address block := []
+  let sliceRound := (4: Nat) * w
+  let readyVertices := (do
+    let u <- g.nodes
+    let (_, r, _) := findLog! u
+    guard (r == sliceRound)
+    guard (strong_path v g logs u v)
+    pure v
+  )
+  let prepare := decide $ readyVertices.length ≥ 2 * f + 1
+  if ¬ prepare then return ()
+  let loopWaves := (do
+    let index <- List.range (w-1 - wd)
+    pure $ Fin.ofNat 3 (w-1 - index)
+  )
+  let (leadersStack, lastLeader) <- loopWaves.foldl
+    -- since for-in loop in action does not work well,
+    -- now use `foldl` and `bind` to simulate for-in
+    (fun last w' => last >>= fun ((leadersStack, v): List (vertex address block) × vertex address block) =>
+      getWaveVertexLeader i w' >>= fun v'o =>
+      match v'o with
+      | none => return (leadersStack, v)
+      | some v' =>
+        if strong_path v g logs v' v
+          then return (leadersStack.insert v', v')
+          else return (leadersStack, v)
+    )
+    -- initial action, returns variables in loop
+    $ (pure ([v], v))
+  let mut leadersStack := [v]
+  let mut vLast := v
+  -- for w' in loopWaves veil_do
+  --   let v' <- getWaveVertexLeader i w'
+
+  decidedWave i := w
+}
+
+action advanceRound (i: address) {
+  return ()
+}
 
 -- action test_for {
 --   let mut l: List Nat := []
 --   for i in ([1,2,3]: List Nat) do
 --     l := l.insert i
+-- }
+
+-- action test_match {
+--   match some 2 with
+--     | some n => test_ := n
+--     | none => return ()
 -- }
 #gen_spec
 
@@ -362,6 +505,8 @@ action getWaveVertexLeader (i: address) (w: wave) {
   f := 1
   a_bcast := fun i r => Fin.ofNat 48 (4 * r + i)
 }
+
+-- #check_invariants
 
 end DAG
 
